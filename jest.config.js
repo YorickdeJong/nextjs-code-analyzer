ce4d4aa53e5ba96a4bbf6f56ab0d67885dfa4925
Yorick De Jong <yorickdejong@gmail.com>
2024-02-11 16:40:14 +0100

    fixed testomg suite

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ["<rootDir>/implementations/typescript/package.json"],
+  modulePathIgnorePatterns: ["<rootDir>/implementations/typescript/package.json", ],
+  testPathIgnorePatterns: [
+    "<rootDir>/implementations/typescript/nextjs-code-analyzer-extension/"
+  ]
 };
