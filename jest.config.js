e3a044e26c5b7ea1079f9357732b80c727b6c51b
Yorick De Jong <yorickdejong@gmail.com>
2024-02-20 09:30:53 +0100

    test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ["<rootDir>/implementations/typescript/package.json", ],
+  modulePathIgnorePatterns: ["<rootDir>/implementations/typescript/src/package.json", ],
   testPathIgnorePatterns: [
     "<rootDir>/implementations/typescript/nextjs-code-analyzer-extension/"
   ]
